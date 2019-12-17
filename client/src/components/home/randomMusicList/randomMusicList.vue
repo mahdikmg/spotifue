7cac0eb51b27c360ec349e58710f3a0b7c7c597e
KMG <mahdi.k.m.g.6@gmail.com>
2019-12-17 23:07:18 +0330

    player component play and pause music, change volume and current time of music that is playing.

--- client/src/components/home/randomMusicList/randomMusicList.vue
+++ client/src/components/home/randomMusicList/randomMusicList.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="bg-light-pink radius py-3 pl-3 pr-2">
+    <div class="bg-light-pink radius pt-3 pl-4 pr-2">
         <h5 class="font-weight-bold">Recommended</h5>
-        <ol class="p-0 overflow-auto">
+        <ol class="m-0 p-0 overflow-auto">
             <div class="container-fluid">
                 <template v-for="item in recommended">
                     <li class="row my-4" :key="item.index" v-if="item.preview_url !== null">
                         <div class="col d-flex align-items-center pointer" @click="setPlayingRightNow({ url: item.preview_url,
-                        image: item.album.images[2].url, artist: item.name, track: item.album.artists[0].name })">
+                        image: item.album.images[2].url, artist: item.name, track: item.album.artists[0].name, status: true })">
                             <img :src="item.album.images[2].url" width="32" height="32" class="rounded mr-3">
                             <div>
                                 <small class="font-weight-bold align-top d-block">{{ item.album.artists[0].name }}</small>
--- client/src/components/player/player.vue
+++ client/src/components/player/player.vue
@@ -1,45 +1,86 @@
 <template>
-    <div class="row bg-purple px-5 py-2 player-radius position-fixed w-100 player d-flex align-items-center">
-        <img class="rounded" :src="this.$store.state.playingRightNow.image" width="64" height="64">
+    <div class="row bg-purple px-5 py-2 player-radius position-sticky player d-flex align-items-center">
+        <img class="rounded border-0" :src="playingRightNow.image" width="64" height="64">
         <div class="col-2 text-white">
-            <small class="font-weight-bold align-top d-block">{{ this.$store.state.playingRightNow.track }}</small>
-            <small class="align-baseline d-block">{{ this.$store.state.playingRightNow.artist }}</small>
+            <small class="font-weight-bold align-top d-block">{{ playingRightNow.track }}</small>
+            <small class="align-baseline d-block">{{ playingRightNow.artist }}</small>
         </div>
-        <!--<audio class="col" :src="this.$store.state.playingRightNow.url" autoplay controls></audio>-->
         <div class="col">
             <div class="audio-player row align-items-center">
                 <span class="icon icon-favorite"></span>
                 <div class="col mx-5">
                     <div class="row justify-content-center align-content-center mb-2">
-                        <span class="icon icon-play"></span>
-                        <span class="icon icon-pause"></span>
+                        <span class="icon icon-pause" @click="paused" v-if="playingRightNow.status"></span>
+                        <span class="icon icon-play" @click="played" v-else></span>
+                        <audio class="d-none" id="player" :src="playingRightNow.url" @timeupdate="timeUpdate"
+                               @ended="paused" autoplay></audio>
                     </div>
                     <div class="row align-items-center">
-                        <small class="text-white">2:05</small>
-                        <div class="col">
-                            <div class="progress">
-                                <div class="progress-bar bg-white w-25" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
+                        <small class="text-white">{{ musicProgress | timeFilter }}</small>
+                        <div class="col d-flex align-items-center">
+                            <input type="range" class="w-100 music-progress" min="0" max="30" v-model="musicProgress"
+                            @change="changeMusicTime">
                         </div>
-                        <small class="text-white">3:56</small>
+                        <small class="text-white">0:30</small>
                     </div>
                 </div>
                 <span class="icon icon-volume mr-2"></span>
-                <input type="range" class="volume" min="0" max="100">
+                <input type="range" class="volume" min="0" max="100" @change="volumeChanged" v-model="volumer">
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import { mapState } from 'vuex'
     export default {
         name: "player",
+        data() {
+            return {
+                musicProgress: 0,
+                volumer: 100
+            }
+        },
+        computed: {
+            ...mapState([
+                'playingRightNow'
+            ])
+        },
+        methods: {
+            played() {
+                document.getElementById('player').play()
+                this.playingRightNow.status = true
+            },
+            paused() {
+                document.getElementById('player').pause()
+                this.playingRightNow.status = false
+            },
+            timeUpdate() {
+                this.musicProgress = document.getElementById('player').currentTime
+            },
+            changeMusicTime() {
+                document.getElementById('player').currentTime = this.musicProgress
+            },
+            volumeChanged() {
+                document.getElementById('player').volume = this.volumer / 100
+            }
+        },
+        filters: {
+            timeFilter(sec) {
+                if (sec < 10) {
+                    return '0:0' + Math.floor(sec)
+                } else {
+                    return '0:' + Math.floor(sec)
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
     .player {
         bottom: 0;
+        margin-top: auto;
     }
 
     .bg-purple {
@@ -50,21 +91,14 @@
         border-radius: 15px 15px 0 0;
     }
 
-    audio:focus {
-        outline: none !important;
-    }
-
-    .progress {
-        height: 2px;
-        background-color: darkgrey;
-    }
-
     /* edit input range for sound volume - cross browser compatible */
     input[type=range] {
         height: 2px;
         -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
-        width: 100px; /* Specific width is required for Firefox. */
         background: transparent;
+    }
+    input[type=range].volume {
+        width: 100px; /* Specific width is required for Firefox. */
     }
     input[type=range]::-webkit-slider-thumb {
         -webkit-appearance: none;
@@ -73,12 +107,14 @@
         outline: none;
     }
     input[type=range]::-ms-track {
-        width: 100px;
         cursor: pointer;
         /* Hides the slider so custom styles can be added */
         background: transparent;
         border-color: transparent;
         color: transparent;
+    }
+    input[type=range].volume::-ms-track {
+        width: 100px;
     }
 
     input[type=range]::-webkit-slider-thumb {
@@ -107,6 +143,9 @@
         height: 0px;
         cursor: pointer;
         box-shadow: -80px 0 0 80px white;
+    }
+    input[type='range'].music-progress::-webkit-slider-thumb {
+        box-shadow: -500px 0 0 500px white;
     }
 
     input[type="range"]::-moz-range-progress {
@@ -142,6 +181,9 @@
         mask-image: url('../../assets/play.svg');
         background-color: white;
     }
+    .icon.icon-play:hover {
+        cursor: pointer;
+    }
 
     .icon.icon-pause {
         height: 1rem;
@@ -149,6 +191,9 @@
         -webkit-mask-image: url('../../assets/pause.svg');
         mask-image: url('../../assets/pause.svg');
         background-color: white;
+    }
+    .icon.icon-pause:hover {
+        cursor: pointer;
     }
 
     .icon.icon-volume {
