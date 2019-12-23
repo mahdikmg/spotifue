ab318503fd48df353587d8b0fede5eab17f4d8bb
KMG <mahdi.k.m.g.6@gmail.com>
2019-12-24 01:47:23 +0330

    deploying spotifue

--- server/src/app.js
+++ server/src/app.js
@@ -8,14 +8,14 @@ app.use(bodyParser.json())
 app.use(cors())
 
 const corsOptions = {
-  origin: 'http://localhost:8080'
+  origin: 'http://localhost:8081'
   // change it to your own origin
 }
 
 const Oauth = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
-    Authorization: 'Basic '
+    Authorization: 'Basic Y2E4YTMwN2NmMWY3NDIxNzg3MmE2MTJmNTkxY2M5NmE6ZjI2NTgzMDEyN2QyNGNiYmE0ZjRkMWI5YzRiN2Q4ZDQ='
     // encode your client_id and client_secret to base64 in this pattern client_id:client_secret
     // and add it after Basic like 'Basic ZjM4ZjAw...WY0MzE='
   },
@@ -25,17 +25,19 @@ const Oauth = {
   json: true
 }
 
-let token = ''
-request.post(Oauth, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    token = body.access_token
-  } else {
-    token = error
-  }
+app.get('/api', cors(corsOptions), (req, res) => {
+  request.post(Oauth, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      return res.send(body.access_token)
+    } else {
+      return res.send(error)
+    }
+  })
 })
 
-app.get('/api', cors(corsOptions), (request, response) => {
-  return response.send(token)
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname +'/public/'))
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+}
 
 app.listen(process.env.PORT || 8081)
